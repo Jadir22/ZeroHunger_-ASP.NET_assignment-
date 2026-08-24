59b91509d9ef642e81d65bb349d380f65070ab23
Junayed Islam <junayedislamjadir5721@gmail.com>
2026-08-24 23:01:38 +0600

    Added CRUD API controllers

---
+++ ZeroHunger/AppLayer/Controllers/RestaurantController.cs
@@ -0,0 +1,54 @@
+﻿using BLL.Models;
+using BLL.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        RestaurantService service;
+
+        public RestaurantController(RestaurantService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("all")]
+        public IActionResult All()
+        {
+            var data = service.Get();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var data = service.Get(id);
+            return Ok(data);
+        }
+
+        [HttpPost("create")]
+        public IActionResult Create(RestaurantModel model)
+        {
+            var data = service.Create(model);
+            return Ok(data);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update(RestaurantModel model)
+        {
+            var data = service.Update(model);
+            return Ok(data);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var data = service.Delete(id);
+            return Ok(data);
+        }
+    }
+}
